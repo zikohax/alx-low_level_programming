3412a8986c078178eb2caa8e438f6b8fdc0cf7a3
zikohax <zikohax@gmail.com>
2023-06-16 14:37:32 -0700

    #advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,14 @@
   *
   * Return: Always (Success)
   */
-int main() {
+int main() 
+{
     int i, j;
 
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
+    for (i = 0; i < 100; i++) 
+    {
+        for (j = i; j < 100; j++) 
+	{
             int first_digit_i = i / 10;
             int second_digit_i = i % 10;
             int first_digit_j = j / 10;
@@ -22,7 +25,8 @@ int main() {
             putchar(first_digit_j + '0');
             putchar(second_digit_j + '0');
 
-            if (i != 99 || j != 99) {
+            if (i != 99 || j != 99) 
+	    {
                 putchar(',');
                 putchar(' ');
             }
