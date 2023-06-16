14157f83d5412d3ddb12f2c585dc2bb0f1ff1f12
zikohax <zikohax@gmail.com>
2023-06-15 18:08:16 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 /**
- * should return 1
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
-
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
