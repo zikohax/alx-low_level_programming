26e451fe3e857a67fa1c12c8840edef7a65e3a57
zikohax <zikohax@gmail.com>
2023-06-16 14:35:33 -0700

    #advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+/**
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
+int main() {
+    int i, j;
+
+    for (i = 0; i < 100; i++) {
+        for (j = i; j < 100; j++) {
+            int first_digit_i = i / 10;
+            int second_digit_i = i % 10;
+            int first_digit_j = j / 10;
+            int second_digit_j = j % 10;
+
+            putchar(first_digit_i + '0');
+            putchar(second_digit_i + '0');
+            putchar(' ');
+            putchar(first_digit_j + '0');
+            putchar(second_digit_j + '0');
+
+            if (i != 99 || j != 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    return 0;
+}
+
