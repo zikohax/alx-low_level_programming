247eb3991110287a23171c5875f5e705ae8b35cb
zikohax <zikohax@gmail.com>
2023-06-15 17:39:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #include<stdio.h>
-int main(){puts("Programming is like building a multilingual puzzle\n");return 0;}
+int main(){puts("\"Programming is like building a multilingual puzzle\n");return 0;}
