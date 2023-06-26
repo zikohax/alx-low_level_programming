695e9a60684ea9fb2f47b92a7ed5176eccec2d08
MEKKAOUI Zakaria <zikohax@gmail.com>
2023-06-26 15:54:21 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,17 +6,15 @@
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];  // +1 for null terminator
-
-    srand(time(NULL));  // Seed the random number generator with current time
+    char password[PASSWORD_LENGTH + 1];
+    srand(time(NULL));
 
     for (int i = 0; i < PASSWORD_LENGTH; i++)
     {
-        // Generate a random character between '!' and '~'
         password[i] = '!' + (rand() % 94);
     }
 
-    password[PASSWORD_LENGTH] = '\0';  // Null terminate the password string
+    password[PASSWORD_LENGTH] = '\0';
 
     printf("%s", password);
 
