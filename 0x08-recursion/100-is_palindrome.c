3adda5bb426f80cb8781d5c004603a014b6df400
MEKKAOUI Zakaria <zikohax@gmail.com>
2023-07-05 18:53:05 -0700

    optional tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,9 +10,9 @@
  */
 int palind2(char *a, int l)
 {
-    if (*a == 0)
-        return (l - 1);
-    return (palind2(a + 1, l + 1));
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
 }
 /**
  * palind3 - compares string vs string reverse
@@ -25,11 +25,11 @@ int palind2(char *a, int l)
 
 int palind3(char *a, int l)
 {
-    if (*a != *(a + l))
-        return (0);
-    else if (*a == 0)
-        return (1);
-    return (palind3(a + 1, l - 2));
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
 }
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -40,8 +40,8 @@ int palind3(char *a, int l)
  */
 int is_palindrome(char *s)
 {
-    int l;
+	int l;
 
-    l = palind2(s, 0);
-    return (palind3(s, l));
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
