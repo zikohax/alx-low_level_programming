935d60ba1b267a33a71ae79735c7b06b9e396386
MEKKAOUI ZAKARIA <zikohax@gmail.com>
2023-07-17 16:26:42 -0700

    fixing bugs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,25 @@
-#ifndef _dog_h_
-#define _dog_h_
-
+#ifndef STRUCTS
+#define STRUCTS
 /**
- * struct dog - such structure, wow
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
-
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
-#endif /* _dog_h_ */
+#endif
