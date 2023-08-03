ffa26bceff2cb842df3b2d2cc28d12169dea2220
MEKKAOUI Zakaria <zikohax@gmail.com>
2023-08-03 16:16:06 -0700

    mandatory tasks

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
